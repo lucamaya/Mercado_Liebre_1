a2aa55f1d8dce7525093a8bf31e09bab3d6edc04
lucamaya <lukitasamaya21@gmail.com>
2022-07-09 22:46:24 -0300

    subiendo cambios

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
+
 const port = 3030;
 
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
